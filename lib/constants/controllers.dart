2dcfef249b364ad028ef08ea42662c2b6abc2c56
Hope Kagame <kagame458@protonmail.com>
2023-03-12 21:04:19 +0300

    Add new files

--- lib/constants/controllers.dart
+++
@@ -1,5 +0,0 @@
-import 'package:webdashboard/controllers/menu_controller.dart';
-import 'package:webdashboard/controllers/navigation_controller.dart';
-
-XMenuController menuController = XMenuController.instance;
-NavigationController navigationController = NavigationController.instance;
--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,35 @@
+import '../../lib/constants.dart';
+import '../../lib/controllers/MenuAppController.dart';
+import '../../lib/screens/main/main_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:webdashboard/constants/style.dart';
-import 'package:webdashboard/controllers/menu_controller.dart';
-import 'package:webdashboard/controllers/navigation_controller.dart';
-import 'package:webdashboard/layout.dart';
-import 'package:webdashboard/pages/404/error.dart';
-import 'package:webdashboard/pages/authentication/authentication.dart';
-import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-import 'routing/routes.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  Get.put(XMenuController());
-  Get.put(NavigationController());
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      initialRoute: authenticationPageRoute,
-      unknownRoute: GetPage(
-          name: '/not-found',
-          page: () => const PageNotFound(),
-          transition: Transition.fadeIn),
-      getPages: [
-        GetPage(
-            name: rootRoute,
-            page: () {
-              return SiteLayout();
-            }),
-        GetPage(
-            name: authenticationPageRoute,
-            page: () => const AuthenticationPage()),
-      ],
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Dashboard',
-      theme: ThemeData(
-        scaffoldBackgroundColor: light,
-        textTheme: GoogleFonts.mulishTextTheme(Theme.of(context).textTheme)
-            .apply(bodyColor: Colors.black),
-        pageTransitionsTheme: const PageTransitionsTheme(builders: {
-          TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
-          TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
-        }),
-        primarySwatch: Colors.blue,
+      title: 'Flutter Admin Panel',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
       ),
-      // home: AuthenticationPaMenuControllerMenuControllerge(),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => MenuAppController(),
+          ),
+        ],
+        child: MainScreen(),
+      ),
     );
   }
 }
