e05711ac4cd31a26be13a38e85390251bb4c109a
Hope Kagame <kagame458@protonmail.com>
2023-03-12 19:39:50 +0300

    Remove everything

--- lib/constants/controllers.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_web_dashboard/controllers/menu_controller.dart';
-import 'package:flutter_web_dashboard/controllers/navigation_controller.dart';
-
-XMenuController menuController = XMenuController.instance;
-NavigationController navigationController = NavigationController.instance;
