80feb2fb2aa5460654ffe968e19c9a3c4708b484
Hope Kagame <kagame458@protonmail.com>
2023-03-23 22:28:50 +0300

    Update main

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hikers_dash/firebase_options.dart';
 import 'package:hikers_dash/sections/home_page.dart';
+import 'package:hikers_dash/services/auth.dart';
+import 'package:hikers_dash/services/auth_notifer.dart';
+import 'package:hikers_dash/services/models/client.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,12 +20,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'HikersDash',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context) => AuthNotifier(),
+      child: StreamProvider<Client?>.value(
+        initialData: null,
+        value: AuthService().auth,
+        child: MaterialApp(
+          title: 'HikersDash',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const MyHomePage(),
+        ),
       ),
-      home: const MyHomePage(),
     );
   }
 }
