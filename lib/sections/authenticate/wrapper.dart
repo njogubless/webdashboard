4b2c1d80aaa3438d53cf3b51c63b448a38d287bd
Hope Kagame <kagame458@protonmail.com>
2023-03-23 22:12:52 +0300

    Add wrapper

---
+++ lib/sections/authenticate/wrapper.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:hikers_dash/sections/authenticate/sign_in.dart';
+import 'package:hikers_dash/sections/home_page.dart';
+import 'package:hikers_dash/services/models/client.dart';
+import 'package:provider/provider.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Return either Home or Authentication widget
+
+    // Using the data from the Provider
+    final userAuth = Provider.of<Client?>(context);
+
+    // Return either Home() or Authenticate()
+    if (userAuth == null) {
+      return SignIn();
+    } else {
+      return MyHomePage();
+    }
+  }
+}
