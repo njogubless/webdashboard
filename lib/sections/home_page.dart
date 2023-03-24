53deddcdb8c59fd1be9d2efe7fd245b49d27cb3f
Hope Kagame <kagame458@protonmail.com>
2023-03-24 11:01:10 +0300

    Update layout

--- lib/sections/home_page.dart
+++ lib/sections/home_page.dart
@@ -43,45 +43,49 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: Row(
         children: [
-          NavigationRail(
-            onDestinationSelected: (index) => setState(() {
-              _selectedIndex = index;
-            }),
-            minWidth: 100,
-            elevation: 1,
-            useIndicator: true,
-            indicatorColor: Colors.cyanAccent,
-            labelType: NavigationRailLabelType.all,
-            destinations: const <NavigationRailDestination>[
-              NavigationRailDestination(
-                icon: Icon(Icons.house_outlined),
-                selectedIcon: Icon(Icons.house),
-                label: Text('Home'),
-              ),
-              NavigationRailDestination(
-                icon: Icon(Icons.verified_user_outlined),
-                selectedIcon: Icon(Icons.verified_user),
-                label: Text('Verify user'),
-              ),
-              NavigationRailDestination(
-                icon: Icon(Icons.event_note_outlined),
-                selectedIcon: Icon(Icons.event_note),
-                label: Text('Add Events'),
-              ),
-              NavigationRailDestination(
-                icon: Icon(Icons.event_note_outlined),
-                selectedIcon: Icon(Icons.event_note),
-                label: Text('Manage Events'),
-              ),
-              NavigationRailDestination(
-                icon: Icon(Icons.event_available_outlined),
-                selectedIcon: Icon(Icons.event_available),
-                label: Text('Booked Events'),
-              ),
-            ],
-            selectedIndex: _selectedIndex,
+          Flexible(
+            child: NavigationRail(
+              onDestinationSelected: (index) => setState(() {
+                _selectedIndex = index;
+              }),
+              minWidth: 100,
+              elevation: 1,
+              useIndicator: true,
+              indicatorColor: Colors.cyanAccent,
+              labelType: NavigationRailLabelType.all,
+              destinations: const <NavigationRailDestination>[
+                NavigationRailDestination(
+                  icon: Icon(Icons.house_outlined),
+                  selectedIcon: Icon(Icons.house),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.verified_user_outlined),
+                  selectedIcon: Icon(Icons.verified_user),
+                  label: Text('Verify user'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.event_note_outlined),
+                  selectedIcon: Icon(Icons.event_note),
+                  label: Text('Add Events'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.event_note_outlined),
+                  selectedIcon: Icon(Icons.event_note),
+                  label: Text('Manage Events'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.event_available_outlined),
+                  selectedIcon: Icon(Icons.event_available),
+                  label: Text('Booked Events'),
+                ),
+              ],
+              selectedIndex: _selectedIndex,
+            ),
           ),
-          _showSection(_selectedIndex),
+          Flexible(
+            flex: 7,
+            child: _showSection(_selectedIndex)),
         ],
       ),
     );
